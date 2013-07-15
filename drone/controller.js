93e9372d8ee648d43a0af29e388ba84dbfa888cc
Nick Breen <nick@nickbreen.ca>
2013-07-15 10:18:16 -0400

    Added a few more buttons and appropriate listeners. Small amount of refactoring.

--- drone/controller.js
+++ drone/controller.js
@@ -37,15 +37,50 @@ io.sockets.on('connection', function (socket) {
         }
         if(data.name=="wave"){
             console.log("Browser asked Ar Drone to wave");
-            client.animate('wave', 3500);
+            client.animate('wave', 1500);
         }
         if(data.name=="led"){
             console.log("Browser asked Ar Drone to led");
-              client.animateLeds('doubleMissile', 5, 7);
+            client.animateLeds('doubleMissile', 5, 7);
         }
         if(data.name=="reset"){
             console.log("Browser asked Ar Drone to reset");
-              client.disableEmergency();
+            client.disableEmergency();
         }
+        if(data.name=="config"){
+            console.log("Browser asked Ar Drone to config");
+            client.calibrate(0);
+        }
+              
+        // Movement
+              
+              if(data.name=="front"){
+              console.log("Browser asked Ar Drone to go forward");
+              client.front(0.2);
+              }
+              
+              if(data.name=="left"){
+              console.log("Browser asked Ar Drone to go left");
+              client.left(0.2);
+              }
+              
+              if(data.name=="right"){
+              console.log("Browser asked Ar Drone to right ");
+              client.right(0.2);
+              }
+              
+              if(data.name=="back"){
+              console.log("Browser asked Ar Drone to back");
+              client.back(0.2);
+              }
+              
+              if(data.name=="raise"){
+              console.log("raise");
+              client.up(0.2);
+              }
+              if(data.name=="lower"){
+              console.log("raise");
+              client.down(0.2);
+              }
     });
 });
